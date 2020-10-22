132ad9d2532118ac85755ce5229165f3695ccf3a
J <32556193+jo-code@users.noreply.github.com>
2020-10-22 16:59:08 +0200

    first minimal video stream example

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-# bbb_app
+# App for BigBlueButton
 
-BigBlueButton client app
+This app uses BigBlueButton and is not endorsed or certified by BigBlueButton Inc. BigBlueButton and the BigBlueButton Logo are trademarks of BigBlueButton Inc.
+
+This project is a Flutter application.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+### How to run this first minimal example
 
-A few resources to get you started if this is your first Flutter project:
+1. Start a BBB session in Chrome.
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+2. Login to the same BBB session in another Browser and start sharing your webcam.
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+3. Use Debugger Tools in Chrome. Copy the following strings into the main.dart file:
+
+![alt text](https://github.com/bennyboer/bbb_app/raw/master/doc/url.png)
+
+![alt text](https://github.com/bennyboer/bbb_app/raw/master/doc/offer.png)
+
+4. Run the Flutter App with Debugger in Android Studio.
+
+5. Execute a Hot Reload in Android Studio. Then you should see the video stream in the app.
--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,208 @@
+import 'dart:convert';
 import 'dart:core';
 
+import 'package:bbb_app/src/utils/websocket.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
-void main() => runApp(new BBBApp());
+SimpleWebSocket _socket;
+RTCPeerConnection _pc;
+RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+
+Future main() async {
+  runApp(new BBBApp());
+}
 
 class BBBApp extends StatefulWidget {
   @override
   _BBBAppState createState() => new _BBBAppState();
 }
 
-enum DialogDemoAction {
-  cancel,
-  connect,
-}
-
 class _BBBAppState extends State<BBBApp> {
+
   @override
-  initState() {
+  void initState() {
+
     super.initState();
+
+    _remoteRenderer.initialize().then((value) =>
+        connect()
+    );
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'BBB App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('BBB App'),
-        ),
-        body: Center(
-          child: Text('To-Do!'),
-        ),
-      ),
+      home: VideoPage(),
     );
   }
+
+
+//TODO custom TURN/STUN server returned by BBB
+  Map<String, dynamic> _iceServers = {
+    'iceServers': [
+      {'url': 'stun:stun.l.google.com:19302'},
+    ]
+  };
+
+  final Map<String, dynamic> _config = {
+    'mandatory': {},
+    'optional': [
+      {'DtlsSrtpKeyAgreement': true},
+    ],
+  };
+
+  final Map<String, dynamic> _constraints = {
+    'mandatory': {
+      'OfferToReceiveAudio': true,
+      'OfferToReceiveVideo': true,
+    },
+    'optional': [],
+  };
+
+  void connect() async {
+
+    print("connect!!!");
+
+    var url = 'https://bbbXXXXXXX.cs.hm.edu/bbb-webrtc-sfu?sessionToken=XXXXXXXXXX';
+    _socket = SimpleWebSocket(url);
+
+    print('connect to $url');
+
+    _socket.onOpen = () {
+      print('onOpen');
+      createOffer();
+    };
+
+    _socket.onMessage = (message) {
+      print('Received data: ' + message);
+      JsonDecoder decoder = new JsonDecoder();
+      onMessage(decoder.convert(message));
+    };
+
+    _socket.onClose = (int code, String reason) {
+      print('Closed by server [$code => $reason]!');
+    };
+
+    await _socket.connect();
+  }
+
+  send(data) {
+    JsonEncoder encoder = new JsonEncoder();
+    _socket.send(encoder.convert(data));
+  }
+
+  void onMessage(message) async {
+    switch (message['id']) {
+
+      case 'startResponse':
+        {
+          await _pc.setRemoteDescription(
+              new RTCSessionDescription(message['sdpAnswer'], 'answer'));
+        }
+        break;
+
+      case 'iceCandidate':
+        {
+          RTCIceCandidate candidate = new RTCIceCandidate(
+              message['candidate']['candidate'],
+              message['candidate']['sdpMid'],
+              message['candidate']['sdpMLineIndex']
+          );
+          await _pc.addCandidate(candidate);
+        }
+        break;
+
+
+      case 'playStart':
+        {
+          print("############################################## playStart");
+          print(_pc.getRemoteStreams());
+          _remoteRenderer.srcObject = _pc.getRemoteStreams()[1];
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  createOffer() async {
+    try {
+      _pc = await createPeerConnection(_iceServers, _config);
+
+      _pc.onIceCandidate = (candidate) {
+        send({
+          'cameraId': 'XXXXXX',
+          'candidate': {
+            'candidate': candidate.candidate,
+            'sdpMLineIndex': candidate.sdpMlineIndex,
+            'sdpMid': candidate.sdpMid,
+          },
+          'id': 'onIceCandidate',
+          'role': 'viewer',
+          'type': 'video'
+        });
+      };
+
+      //this is never triggered.... using case 'playStart' in onMessage() instead
+      _pc.onAddStream = (stream) {
+        print("############################################## onAddStream");
+        _remoteRenderer.srcObject = stream;
+      };
+
+      _pc.onRemoveStream = (stream) {
+        print("############################################## onRemoveStream");
+        _remoteRenderer.srcObject = null;
+      };
+
+      _pc = await createPeerConnection(_iceServers, _config);
+
+      RTCSessionDescription s = await _pc.createOffer(_constraints);
+      _pc.setLocalDescription(s);
+
+      send({
+        'bitrate': 200,
+        'cameraId': 'XXXXXX',
+        'id': 'start',
+        'meetingId': 'XXXXXX',
+        'record': true,
+        'role': 'viewer',
+        'sdpOffer': s.sdp,
+        'type': 'video',
+        'userId': 'XXXXXX',
+        'userName': 'XXXXXX',
+        'voiceBridge': 'XXXXXX',
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+
 }
+
+
+class VideoPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: 300,
+      height: 400,
+      child: RTCVideoView(_remoteRenderer),
+    );
+
+  }
+
+}
+
