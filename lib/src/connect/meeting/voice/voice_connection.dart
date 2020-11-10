fce1fd5657e1f7036eb71416d2148fb33f8d4491
Konstantin <gleydar@outlook.de>
2020-11-10 15:54:53 +0100

    :sparkles: Work on #7

--- lib/src/connect/meeting/voice/voice_connection.dart
+++ lib/src/connect/meeting/voice/voice_connection.dart
@@ -1,74 +1,35 @@
 import 'package:bbb_app/src/connect/meeting/meeting_info.dart';
+import 'package:bbb_app/src/connect/meeting/voice/voice_manager.dart';
 import 'package:sip_ua/sip_ua.dart';
 
-class VoipManager implements SipUaHelperListener {
-
-  SIPUAHelper _helper;
+class VoiceConnection extends VoiceManager implements SipUaHelperListener {
   MeetingInfo info;
-  int audioSessionNumber = 1;
 
-  VoipManager(this.info) {
-    _helper = new SIPUAHelper();
-    _helper.addSipUaHelperListener(this);
+  VoiceConnection(this.info) : super(null) {
+    helper.addSipUaHelperListener(this);
   }
 
-  String _getNakedUrl() {
-    return Uri.parse(info.joinUrl)
-        .replace(queryParameters: null, scheme: null).toString();
-  }
-
-  String _buildUser() {
-    return "${info.internalUserID}_$audioSessionNumber-${info.fullUserName}@${_getNakedUrl()}";
-  }
-
-  Uri _buildWsUri(Uri joinUrl) {
-    return joinUrl.replace(path: "ws").replace(queryParameters: {"sessionToken": info.sessionToken})
-        .replace(scheme: "wss");
-  }
-
-  Map<String, dynamic> _createCookies() {
-    Map<String, dynamic> cookies = new Map();
-    cookies["Cookie"] = info.cookie.split(";").where((element) => element.contains("JSESSIONID"));
-    return cookies;
-  }
-
-  String _buildEcho() {
-    return "echo${info.voiceBridge}@${_getNakedUrl()}";
-  }
-
-  void start() {
-    UaSettings settings = UaSettings();
-
-    settings.webSocketUrl = _buildWsUri(Uri.parse(info.joinUrl)).toString();
-    settings.webSocketSettings.extraHeaders = _createCookies();
-    settings.webSocketSettings.allowBadCertificate = true;
-    settings.webSocketSettings.userAgent = 'Dart/2.8 (dart:io) for OpenSIPS.';
-
-    settings.uri = _buildEcho();
-    settings.authorizationUser = _buildUser();
-    settings.displayName = info.fullUserName;
-    settings.userAgent = 'Dart SIP Client v1.0.0';
-
-    _helper.start(settings);
+  void connect() {
+    helper.start(super.buildSettings());
   }
 
   @override
   void callStateChanged(Call call, CallState state) {
+    print("[SIP] Call state changed, is now ${state.toString()}");
   }
 
   @override
   void onNewMessage(SIPMessageRequest msg) {
-    // TODO: implement onNewMessage
+    print("[SIP] New Message: ${msg.toString()}");
   }
 
   @override
   void registrationStateChanged(RegistrationState state) {
-    // TODO: implement registrationStateChanged
+    print("[SIP] Registration Changed: ${state.toString()}");
   }
 
   @override
   void transportStateChanged(TransportState state) {
-    // TODO: implement transportStateChanged
+    print("[SIP] Transport Changed: ${state.toString()}");
   }
-
 }
