c46d62b91d939178313e0bf85b838641ddeba2fd
Konstantin <gleydar@outlook.de>
2020-11-10 20:18:55 +0100

    :sparkles: Work on #7, current status: Stuck in Echo Test, but we hear ourselves!

--- lib/src/connect/meeting/main_websocket/voice/voice_module.dart
+++ lib/src/connect/meeting/main_websocket/voice/voice_module.dart
@@ -2,6 +2,7 @@ import 'package:bbb_app/src/connect/meeting/main_websocket/module.dart';
 import 'package:bbb_app/src/connect/meeting/meeting_info.dart';
 import 'package:bbb_app/src/connect/meeting/voice/voice_connection.dart';
 
+/// This Module handles Voice Stream Initialisation and sends the EchoTest end message
 class VoiceModule extends Module {
   MeetingInfo _info;
   VoiceConnection _connection;
@@ -10,8 +11,18 @@ class VoiceModule extends Module {
     _connection = new VoiceConnection(_info, this);
   }
 
-  /// {"msg":"method","method":"logClient","params":["info","Audio Joined","audio_joined",{"clientURL":"https://bbb9.cs.hm.edu/html5client/join?sessionToken=f8dvpv0dyx7yn8mc"},{"sessionToken":"f8dvpv0dyx7yn8mc","meetingId":"027c9467b50f9b06ee4ea492a08fa14becaceb18-1605033005419","requesterUserId":"w_bg8ww0dpizn1","fullname":"Schlosser, Konstantin","confname":"Startraum","externUserID":"gl-kzsuivppxpyf","uniqueClientSession":"f8dvpv0dyx7yn8mc-872sc"}],"id":"65"}"
-
+  /// As far as I can tell, this message signals that we are satisfied with the echo test
+  /// {
+  /// "msg":"method","method":"logClient","params":["info","Audio Joined","audio_joined",
+  /// {"clientURL":"https://bbb9.cs.hm.edu/html5client/join?sessionToken=f8dvpv0dyx7yn8mc"},
+  /// {"sessionToken":"f8dvpv0dyx7yn8mc","meetingId":"027c9467b50f9b06ee4ea492a08fa14becaceb18-1605033005419",
+  ///  "requesterUserId":"w_bg8ww0dpizn1",
+  ///  "fullname":"Schlosser, Konstantin",
+  ///  "confname":"Startraum",
+  ///  "externUserID":"gl-kzsuivppxpyf",
+  ///  "uniqueClientSession":"f8dvpv0dyx7yn8mc-872sc"}],
+  ///  "id":"65"
+  ///  }"
   void endEchoTest() {
     sendMessage({
       "msg": "method",
@@ -20,10 +31,7 @@ class VoiceModule extends Module {
         "info",
         "Audio Joined",
         "audio_joined",
-        {
-          "clientURL":
-              _info.joinUrl
-        },
+        {"clientURL": _info.joinUrl},
         {
           "sessionToken": _info.sessionToken,
           "meetingId": _info.meetingID,
@@ -31,7 +39,9 @@ class VoiceModule extends Module {
           "fullname": _info.fullUserName,
           "confname": _info.conferenceName,
           "externUserID": _info.externUserID,
-          "uniqueClientSession": "${_info.sessionToken}-${_connection.audioSessionNumber}"
+          // I have no idea on how they generate this, but... I guess that will have to do for now
+          "uniqueClientSession":
+              "${_info.sessionToken}-${_connection.audioSessionNumber}"
         }
       ]
     });
@@ -44,11 +54,9 @@ class VoiceModule extends Module {
 
   @override
   Future<void> onDisconnect() {
-    // TODO: implement onDisconnect
+    _connection.disconnect();
   }
 
   @override
-  void processMessage(Map<String, dynamic> msg) {
-    // TODO: implement processMessage
-  }
+  void processMessage(Map<String, dynamic> msg) {}
 }
