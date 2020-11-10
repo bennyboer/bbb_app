eeb46ec71a511d536296653181715ccc1ebe035a
Konstantin <gleydar@outlook.de>
2020-11-10 15:46:44 +0100

    :sparkles: Work on #7

--- lib/src/connect/meeting/voice/voice_connection.dart
+++ lib/src/connect/meeting/voice/voice_connection.dart
@@ -12,19 +12,40 @@ class VoipManager implements SipUaHelperListener {
     _helper.addSipUaHelperListener(this);
   }
 
-  String buildUser() {
-    return "${info.internalUserID}_$audioSessionNumber-${info.fullUserName}@${Uri.parse(info.joinUrl)
-        .replace(queryParameters: null)}";
+  String _getNakedUrl() {
+    return Uri.parse(info.joinUrl)
+        .replace(queryParameters: null, scheme: null).toString();
+  }
+
+  String _buildUser() {
+    return "${info.internalUserID}_$audioSessionNumber-${info.fullUserName}@${_getNakedUrl()}";
+  }
+
+  Uri _buildWsUri(Uri joinUrl) {
+    return joinUrl.replace(path: "ws").replace(queryParameters: {"sessionToken": info.sessionToken})
+        .replace(scheme: "wss");
+  }
+
+  Map<String, dynamic> _createCookies() {
+    Map<String, dynamic> cookies = new Map();
+    cookies["Cookie"] = info.cookie.split(";").where((element) => element.contains("JSESSIONID"));
+    return cookies;
+  }
+
+  String _buildEcho() {
+    return "echo${info.voiceBridge}@${_getNakedUrl()}";
   }
 
   void start() {
     UaSettings settings = UaSettings();
 
-    settings.webSocketUrl = info.webVoiceConf + "?sessionToken=" + info.sessionToken;
+    settings.webSocketUrl = _buildWsUri(Uri.parse(info.joinUrl)).toString();
+    settings.webSocketSettings.extraHeaders = _createCookies();
     settings.webSocketSettings.allowBadCertificate = true;
     settings.webSocketSettings.userAgent = 'Dart/2.8 (dart:io) for OpenSIPS.';
 
-    settings.uri = buildUser();
+    settings.uri = _buildEcho();
+    settings.authorizationUser = _buildUser();
     settings.displayName = info.fullUserName;
     settings.userAgent = 'Dart SIP Client v1.0.0';
 
@@ -33,7 +54,6 @@ class VoipManager implements SipUaHelperListener {
 
   @override
   void callStateChanged(Call call, CallState state) {
-    // TODO: implement callStateChanged
   }
 
   @override
